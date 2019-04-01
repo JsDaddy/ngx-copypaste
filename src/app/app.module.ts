2a0164dc669dab7d877280d08a53ea0689dd3534
Thegrep01 <den.belotserkovets@gmail.com>
2019-04-01 16:38:21 +0300

    add demo page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {NgxCopyPasteModule} from './ngx-copypaste/ngx-copypaste.module';
-
+import { NgxCopyPasteModule } from './ngx-copypaste/ngx-copypaste.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    NgxCopyPasteModule
+    NgxCopyPasteModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    FormsModule,
+    MatButtonModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
