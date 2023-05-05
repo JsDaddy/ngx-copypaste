1b8d52424629befb773ffbae897423cec7f9c282
andriikamaldinov1 <akam@jsdaddy.io>
2023-05-05 19:03:15 +0300

    fix(no-ref): fix frames

--- src/app/shared/copied/copied.component.ts
+++ src/app/shared/copied/copied.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { NgClass, NgOptimizedImage } from '@angular/common';
 import { AssetPipe } from '@libraries/asset/asset.pipe';
 import { SharedAssetPath } from './copied.path';
+import { delay, of, takeUntil } from 'rxjs';
+import { UnSubscriber } from '@libraries/unsubscriber/unsubscriber.service';
 
 @Component({
     selector: 'jsdaddy-open-source-copied-button',
     standalone: true,
-    imports: [NgOptimizedImage, AssetPipe],
+    imports: [NgOptimizedImage, AssetPipe, NgClass],
     templateUrl: './copied.component.html',
     styleUrls: ['./copied.component.scss'],
 })
-export class CopiedComponent {
+export class CopiedComponent extends UnSubscriber {
     public readonly assetPath = SharedAssetPath.COPIED;
+    public copyButtonClass = 'non-active';
+
+    public copyText(): void {
+        this.copyButtonClass = 'active';
+        of({})
+            .pipe(delay(1000), takeUntil(this.unsubscribe$$))
+            .subscribe(() => {
+                this.copyButtonClass = 'non-active';
+            });
+    }
 }
