e8d0f5d96304b29e85e9ee7a967a4a5e819e0a42
andriikamaldinov1 <akam@jsdaddy.io>
2024-11-21 15:20:08 +0200

    fix(ref: no-ref): updage ng 19

--- projects/ngx-copypaste-lib/src/test/copy.cy-spec.ts
+++ projects/ngx-copypaste-lib/src/test/copy.cy-spec.ts
@@ -1,11 +1,8 @@
-import { CypressTestMaskModule } from './utils/cypress-test.module';
 import { CypressTestMaskComponent } from './utils/cypress-test.component';
 
 describe('Test Date Hh:m0', () => {
     it('should return value from ctrl+V', () => {
-        cy.mount(CypressTestMaskComponent, {
-            imports: [CypressTestMaskModule],
-        });
+        cy.mount(CypressTestMaskComponent);
 
         cy.get('#masked').type('testing').should('have.value', 'testing');
         cy.get('#btn').click();
